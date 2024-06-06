1a9f1aa7957bd1225b978e20d5a95c59c931de38
CTYeyo <luisdiegoyeyo3@gmail.com>
2024-06-06 05:07:41 +0000

    Mundo guardado 2024-06-06

--- servidor.py
+++ servidor.py
@@ -1,8 +1,3 @@
-
-
-
-
-
 # Custom version
 # mohist / catserver   Install forge   first
 # purpur               Install fabric  first
@@ -19,13 +14,6 @@
 # sa		  South America (São Paulo)
 # us		  United States (Ohio)
 # us-cal-1	  United States (California)
-
-
-
-
-
-
-
 
 
 
